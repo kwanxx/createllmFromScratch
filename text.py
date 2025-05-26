84905826ce8360d9b9a7a829cfb2f0eb7bace690
kwanxx <845367559@qq.com>
2025-05-26 11:43:58 +0800

    20250526 first commit

--- text.py
+++ text.py
@@ -1,5 +1,6 @@
 import urllib.request
 import re
+from SimpleTokenizerV1 import *
 '''
 第一部分
 1、下载文本
@@ -21,7 +22,7 @@ def openFile(file):
     with open(file, "r", encoding="utf-8" ) as f:
         raw_text = f.read()
     print("文本总字数： ", len(raw_text))
-    # 获取钱100个字符
+    # 获取前100个字符
     print(raw_text[:99])
 
     return raw_text
@@ -29,7 +30,7 @@ def openFile(file):
 # 分词：分割文本，保留逗号、句号，去除空白符号
 def regText(text):
     print("====分割文本====")
-    # 正则加了括号()，表示分割组，保留被分割的标点符号在数组中。
+    # 正则加了括号()的作用：表示分割组，保留被分割的标点符号在数组中。
     result = re.split(r"([,.:;?!_()'\"]|--|\s)",text)
     # 去除空白字符
     result = [r for r in result if r.strip()]
@@ -39,15 +40,50 @@ def regText(text):
     print(result[:30])
     return result
 
-# 排序: 对词组进行去重、排序， set()去重复；sorted()排序
+
+# 排序: 对词组进行去重、排序， set()去重复，sorted()排序
+'''
+set()去重复:
+ a = ["hello","john","alex","hello"]
+ a = set(a)
+ output: a = ["hello","john","alex"]
+'''
 def sortText(result):
     print("====对词组进行排序====")
     sorted_result = sorted(set(result))
     vocab_size = len(sorted_result)
     print(vocab_size)
+    return sorted_result
+'''
+分词阶段
+'''
+# 为词组分配索引，词源ID
+def enumerateText(vocabList):
+    # 通过enumerate()创建词典，自动增加序列
+    vocabDict = {token:integer for integer, token in enumerate(vocabList)}
+    #for i, item in enumerate(vocabDict.items()):
+        # 打印前50项
+        # print(item)
+        # if i >= 50:
+        #     break
+    return vocabDict
+'''
+为了将大语言模型的输出从数值形式转换回文本，
+还需要一种将词元 ID 转换为文本的方法。
+为此，可以创建逆向词汇表，将词元 ID 映射回它们对应的文本词元
+'''
 
-# req()
-# openFile("the-verdict.txt")
-# text = "Hello, I am winky."
-# regText(text)
-sortText(regText(openFile("the-verdict.txt")))
+# 排序的词组
+vocabList = sortText(regText(openFile("the-verdict.txt")))
+# 添加了序号的词组
+vocabDict = enumerateText(vocabList)
+simpleTokenizer = SimpleTokenizerV1(vocabDict)
+text = """
+      "It's the last he painted, you know,"
+       Mrs. Gisburn said with pardonable pride.
+       """
+text = "Hello do you like tea?"
+ids = simpleTokenizer.encode(text)
+text = simpleTokenizer.decode(ids)
+
+print(text)
